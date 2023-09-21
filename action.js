4a173e8f32b5c9233dc4ea2e3d1a12961d4c76ad
Alvaro Sanchez <alvaro_sanchez13@hotmail.com>
2023-09-21 16:42:45 -0300

    Add model

--- action.js
+++ action.js
@@ -9,3 +9,29 @@ $(function () {
         }
     });
 });
+
+
+/*Modal*/
+const exampleModal = document.getElementById('exampleModal')
+if (exampleModal) {
+  exampleModal.addEventListener('show.bs.modal', event => {
+    // Button that triggered the modal
+    const button = event.relatedTarget
+    // Extract info from data-bs-* attributes
+    const recipient = button.getAttribute('data-bs-whatever')
+    // If necessary, you could initiate an Ajax request here
+    // and then do the updating in a callback.
+    const im = button.getAttribute('im')
+    const desc = button.getAttribute('des')
+    // Update the modal's content.
+    const modalTitle = exampleModal.querySelector('.modal-title')
+    const modalBodyInput = exampleModal.querySelector('.modal-body input')
+    const espdes = exampleModal.querySelector('#message-text')
+    const i =exampleModal.querySelector('#imser')
+    
+    i.src = `${im}`
+    espdes.innerHTML = desc
+    modalTitle.textContent = ` ${recipient}`
+    modalBodyInput.value = recipient
+  })
+}
