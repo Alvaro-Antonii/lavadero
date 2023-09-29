31ece62a40447fa3f79694489c4a69bcc470ec65
Alvaro Sanchez <alvaro_sanchez13@hotmail.com>
2023-09-29 19:13:06 -0300

    Efecto p

--- action.js
+++ action.js
@@ -35,3 +35,17 @@ if (exampleModal) {
     modalBodyInput.value = recipient
   })
 }
+
+
+window.onscroll = function(){
+
+  var posicion = window.pageYOffset || document.documentElement.scrollTop;
+
+  var elemento1 = document.getElementById("icon_auto");
+  var elemento2 = document.getElementById("icon_agua");
+  var elemento3 = document.getElementById("letras");
+
+  elemento1.style.bottom = posicion * 0.2 + "px";
+  elemento2.style.top = posicion * 0.1 + "px";
+  elemento3.style.bottom = posicion * 0.4 + "px";
+}
